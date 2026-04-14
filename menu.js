63902b49d7aeaefb952fd9327f46b64d5eeb9619
Fern Bromley <147012078+fernbro@users.noreply.github.com>
2026-04-14 16:45:09 -0700

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,7 +1,19 @@
 (function () {
   "use strict";
-
   $(function () {
-    $(".menu-container").load("menu.html");
+    $(".menu-container").html(`
+      <div class="menu">
+        <div class="menu-table flex-row-space-between">
+          <a class="menu-title" href="index.html" style="font-size: 1.8rem; font-weight: 700;">Fern Bromley</a>
+          <img src="menu.png">
+          <div class="menu-items flex-row-center flex-item">
+            <a class="menu-home" href="index.html">Home</a>
+            <a class="menu-about" href="about.html">About me</a>
+            <a class="menu-research" href="research.html">Research</a>
+            <a href="https://github.com/fernbro">GitHub</a>
+          </div>
+        </div>
+      </div>
+    `);
   });
 })();
