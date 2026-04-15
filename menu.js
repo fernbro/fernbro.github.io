6379d80bbae252a90231e807ec76788e528f0357
Fern Bromley <147012078+fernbro@users.noreply.github.com>
2026-04-15 12:52:59 -0700

    Update menu.js

--- menu.js
+++ menu.js
@@ -10,6 +10,7 @@
             <a class="menu-about" href="about.html">About me</a>
             <a class="menu-research" href="research.html">Research</a>
             <a href="https://github.com/fernbro">GitHub</a>
+            <a href="https://orcid.org/0000-0003-0596-1487">ORCID</a>
           </div>
         </div>
       </div>
