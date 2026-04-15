1a3ff97640a05feaac9ccf2870a2c4c9cea43051
Fern Bromley <147012078+fernbro@users.noreply.github.com>
2026-04-14 18:18:38 -0700

    Update menu.js

--- menu.js
+++ menu.js
@@ -5,7 +5,6 @@
       <div class="menu">
         <div class="menu-table">
           <a class="menu-title" href="index.html" style="font-size: 1.8rem; font-weight: 700;">Fern Bromley</a>
-          <img src="menu.png">
           <div class="menu-items flex-row-center flex-item">
             <a class="menu-home" href="index.html">Home</a>
             <a class="menu-about" href="about.html">About me</a>
